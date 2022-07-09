76de7036047b4de98146e5f082a88e4a46ec362a
dev28k <faizkhan.dev@gmail.com>
2022-07-10 00:28:01 +0500

    q

--- server.js
+++ server.js
@@ -35,10 +35,11 @@ app.use('/vid', express.static(__dirname + '/public/upload/Videos'))
 
 const db = mongoConfig.db;
 
-app.get('/', async function (req, res) {
+app.get('/', function (req, res) {
     var dateTime = new Date();
     try {
-      res.send("Hotel Reservation API"+" "+dateTime+"\n"+JSON.stringify(db.s.namespace)+"\n"+JSON.stringify(mongoConfig.client.connect()))
+      mongoConfig.client.connect()
+      res.send("Hotel Reservation API"+" "+dateTime+"\n"+JSON.stringify(db.s.namespace)+"\n")
     }catch (err) {
       res.send("Connection Error")
     }
